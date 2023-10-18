95ef3d2d77a53a9bd3fa6b4c291655ce915bc0aa
StanM84 <stan1984@abv.bg>
2023-10-18 15:59:50 +0300

    Add project files.

---
+++ ListManipulationAdvanced/ListManipulationAdvanced.cs
@@ -0,0 +1,116 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+string command = Console.ReadLine();
+
+while (command != "end")
+{
+    if (command.StartsWith("Contains"))
+    {
+        int num = int.Parse(command.Split(" ")[1]);
+        bool contains = false;
+
+        foreach (int number in numbers)
+        {
+            if (number == num)
+            {
+                contains = true;
+            }
+        }
+        if (contains)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No such number");
+        }
+    }
+
+    if (command.StartsWith("PrintEven"))
+    {
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                Console.Write(number + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    if (command.StartsWith("PrintOdd"))
+    {
+        foreach(int number in numbers)
+        {
+            if (number %2 != 0)
+            {
+                Console.Write(number + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+    if (command.StartsWith("GetSum"))
+    {
+        int sum = 0;
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine(sum);
+    }
+
+    if (command.StartsWith("Filter"))
+    {
+        string[] commandParts = command.Split(" ");
+        string conditions = commandParts[1];
+        int num = int.Parse(commandParts[2]);
+
+        switch (conditions)
+        {
+            case "<":
+                foreach (int number in numbers)
+                {
+                    if (number < num)
+                    {
+                        Console.Write(number + " ");
+                    }
+                }
+                Console.WriteLine();
+                break;
+             case ">":
+                foreach (int number in numbers)
+                {
+                    if (number > num)
+                    {
+                        Console.Write(number + " ");
+                    }
+                }
+                Console.WriteLine();
+                break;
+            case ">=":
+                foreach (int number in numbers)
+                {
+                    if (number >= num)
+                    {
+                        Console.Write(number + " ");
+                    }
+                }
+                Console.WriteLine();
+                break;
+            case "<=":
+                foreach (int number in numbers)
+                {
+                    if (number <= num)
+                    {
+                        Console.Write(number + " ");
+                    }
+                }
+                Console.WriteLine();
+                break;
+        }
+    }
+
+    command = Console.ReadLine();
+}
+Console.WriteLine(string.Join(" ", numbers));
+
---
+++ ListManipulationBasics/ListManipulationBasics.cs
@@ -0,0 +1,33 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+string command  = Console.ReadLine();
+
+while (command != "end")
+{
+    if (command.StartsWith("Add"))
+    {
+        int numAdd = int.Parse(command.Split(" ")[1]);
+        numbers.Add(numAdd);
+    }
+    else if (command.StartsWith("RemoveAt"))
+    {
+        int indexRemove = int.Parse(command.Split(" ")[1]);
+        numbers.RemoveAt(indexRemove);
+    }
+    else if (command.StartsWith("Remove"))
+    {
+        int numRemove = int.Parse(command.Split(" ")[1]);
+        numbers.Remove(numRemove);
+    }
+    else if (command.StartsWith("Insert"))
+    {
+        int numIsert = int.Parse(command.Split(" ")[1]);
+        int indexInsert = int.Parse(command.Split(" ")[2]);
+        numbers.Insert(indexInsert, numIsert);
+    }
+
+    command = Console.ReadLine();
+}
+Console.WriteLine(string.Join(" ", numbers));
+
+
